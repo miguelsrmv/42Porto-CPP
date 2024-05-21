7203a88d4172fd78688735fe45fda7dfccb3bb79
Miguel <miguel.srmv@gmail.com>
2024-05-21 21:44:22 +0100

    Cleaner

--- CPP Module 00/ex00/megaphone.cpp
+++ CPP Module 00/ex00/megaphone.cpp
@@ -6,20 +6,19 @@
 /*   By: mde-sa-- <mde-sa--@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 21:13:13 by mde-sa--          #+#    #+#             */
-/*   Updated: 2024/05/21 21:39:25 by mde-sa--         ###   ########.fr       */
+/*   Updated: 2024/05/21 21:43:41 by mde-sa--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
-#include <cstring>
 
 int		main(int argc, char **argv)
 {
 	if (argc > 1)
 	{
 		for (int word_index = 1; word_index < argc; word_index++)
-			for (int char_index = 0; char_index < strlen(argv[word_index]); char_index++)
+			for (int char_index = 0; argv[word_index][char_index]; char_index++)
 				std::cout << (char)toupper(argv[word_index][char_index]) ;
 	}
 	else
