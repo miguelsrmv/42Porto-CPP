1ac1114fe2ff107b01001d2d43e64c275ad6d7ec
Miguel <miguel.srmv@gmail.com>
2024-06-02 13:53:37 +0100

    Write to file

--- Module 01/ex04/SedIsForLosers.hpp
+++ Module 01/ex04/SedIsForLosers.hpp
@@ -13,9 +13,8 @@ bool correct_usage (const int argc, const char **argv);
 bool valid_input_file (const char *input_file_path);
 bool valid_output_file (const char *output_file_path);
 
-// Write_to_file.cpp
-void	load_contents_to_buffer(const char *input_file, std::stringstream& buffer);
-void 	write_to_output_file (const char *input_file, std::string &target_file,
-						    std::string& string_1, std::string& string_2);
+// main.cpp
+void write_to_output_file (const char *input_file, const char *output_file,
+						   std::string &string_1, std::string &string_2);
 
 #endif
--- Module 01/ex04/main.cpp
+++ Module 01/ex04/main.cpp
@@ -1,5 +1,29 @@
 #include "SedIsForLosers.hpp"
 
+void
+write_to_output_file (const char *input_file, const char *output_file,
+					  std::string &string_1, std::string &string_2)
+{
+	std::ifstream input_stream (input_file);
+	std::ofstream output_stream (output_file);
+	std::stringstream buffer;
+
+	buffer << input_stream.rdbuf ();
+
+	for (std::string::size_type i = 0; i < buffer.str ().length (); i++)
+		{
+			if (string_1.length ()
+				&& buffer.str ().compare (i, string_1.length (), string_1)
+					   == 0)
+				{
+					output_stream << string_2;
+					i += string_1.length () - 1;
+				}
+			else
+				output_stream << buffer.str ()[i];
+		}
+}
+
 int
 main (int argc, char **argv)
 {
@@ -15,6 +39,8 @@ main (int argc, char **argv)
 
 	std::string string_1 = argv[2];
 	std::string string_2 = argv[3];
-	write_to_output_file ((const char *)argv[1], target_file,
-						  string_1, string_2);
+
+	write_to_output_file ((const char *)argv[1],
+						  (const char *)target_file.c_str (), string_1,
+						  string_2);
 }
