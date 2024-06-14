932121db3b014389fd8f2adf2bc99739f38e950a
Miguel <miguel.srmv@gmail.com>
2024-06-14 10:46:49 +0100

    ex 00 correction

--- Module 00/ex00/megaphone.cpp
+++ Module 00/ex00/megaphone.cpp
@@ -3,26 +3,25 @@
 
 // Returns uppercase version of lowercase_string
 std::string
-upper_string (const std::string& lower_string)
+upper_string(const std::string &lower_string)
 {
 	std::string upper_string;
 
-	for (int i = 0; i < lower_string[i]; i++)
-		upper_string += (char)toupper (lower_string[i]);
+	for (unsigned long i = 0; i < lower_string.size(); i++)
+		upper_string += (char)toupper(lower_string[i]);
 
 	return (upper_string);
 }
 
-int
-main (int argc, char **argv)
+int main(int argc, char **argv)
 {
 	std::string output;
 
 	if (argc > 1)
-		{
-			for (int word_index = 1; word_index < argc; word_index++)
-				output.append (upper_string (argv[word_index])).append (" ");
-		}
+	{
+		for (int word_index = 1; word_index < argc; word_index++)
+			output.append(upper_string(argv[word_index])).append(" ");
+	}
 	else
 		output += "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 
