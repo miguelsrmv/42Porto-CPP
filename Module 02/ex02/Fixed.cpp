f80be513465a4c13f57da4930bdbb10fae2cc488
Miguel <miguel.srmv@gmail.com>
2024-06-15 10:09:51 +0100

    ex02

--- Module 02/ex02/Fixed.cpp
+++ Module 02/ex02/Fixed.cpp
@@ -2,36 +2,41 @@
 
 Fixed::Fixed ()
 {
-	std::cout << "Default constructor called" << std::endl;
+	// std::cout << "Default constructor called" << std::endl;
 	this->fixed_point_number = 0;
 }
 
 Fixed::Fixed (const Fixed &copy)
 {
-	std::cout << "Copy constructor called" << std::endl;
+	// std::cout << "Copy constructor called" << std::endl;
 	*this = copy;
 }
 
 Fixed::Fixed (const int int_parameter)
 {
-	std::cout << "Int constructor called" << std::endl;
+	// std::cout << "Int constructor called" << std::endl;
 	this->fixed_point_number = int_parameter * (1 << this->fractional_bits);
 }
 
 Fixed::Fixed (const float float_parameter)
 {
-	std::cout << "Float constructor called" << std::endl;
+	// std::cout << "Float constructor called" << std::endl;
 	this->fixed_point_number = float_parameter * (1 << this->fractional_bits);
 }
 
 Fixed &
 Fixed::operator= (const Fixed &copy)
 {
-	std::cout << "Copy assignment operator called" << std::endl;
+	// std::cout << "Copy assignment operator called" << std::endl;
 	if (this == &copy)
 		return (*this);
 	this->fixed_point_number = copy.getRawBits ();
 	return (*this);
+}
+
+Fixed::~Fixed ()
+{
+	// std::cout << "Destructor called" << std::endl;
 }
 
 std::ostream &
@@ -44,40 +49,124 @@ operator<< (std::ostream &out, const Fixed &copy)
 bool
 Fixed::operator> (const Fixed &copy)
 {
-	return (this->toFloat () > copy.toFloat ());
+	return (this->getRawBits () > copy.getRawBits ());
 }
 
 bool
 Fixed::operator<(const Fixed &copy)
 {
-	return (this->toFloat () < copy.toFloat ());
+	return (this->getRawBits () < copy.getRawBits ());
 }
 
 bool
 Fixed::operator>= (const Fixed &copy)
 {
-	return (this->toFloat () >= copy.toFloat ());
+	return (this->getRawBits () >= copy.getRawBits ());
 }
 
 bool
 Fixed::operator<= (const Fixed &copy)
 {
-	return (this->toFloat () <= copy.toFloat ());
+	return (this->getRawBits () <= copy.getRawBits ());
 }
 
 bool
 Fixed::operator== (const Fixed &copy)
 {
-	return (this->toFloat () == copy.toFloat ());
+	return (this->getRawBits () == copy.getRawBits ());
 }
 
 bool
 Fixed::operator!= (const Fixed &copy)
 {
-	return (this->toFloat () != copy.toFloat ());
+	return (this->getRawBits () != copy.getRawBits ());
 }
 
-Fixed::~Fixed () { std::cout << "Destructor called" << std::endl; }
+float
+Fixed::operator+ (const Fixed &copy)
+{
+	return (this->toFloat () + copy.toFloat ());
+}
+
+float
+Fixed::operator- (const Fixed &copy)
+{
+	return (this->toFloat () - copy.toFloat ());
+}
+
+float
+Fixed::operator* (const Fixed &copy)
+{
+	return (this->toFloat () * copy.toFloat ());
+}
+
+float
+Fixed::operator/ (const Fixed &copy)
+{
+	return (this->toFloat () / copy.toFloat ());
+}
+
+Fixed &
+Fixed::operator++ ()
+{
+	fixed_point_number++;
+	return (*this);
+}
+
+Fixed
+Fixed::operator++ (int)
+{
+	Fixed this_copy (*this);
+	fixed_point_number++;
+	return (this_copy);
+}
+
+Fixed &
+Fixed::operator-- ()
+{
+	fixed_point_number--;
+	return (*this);
+}
+
+Fixed
+Fixed::operator-- (int)
+{
+	Fixed this_copy (*this);
+	fixed_point_number--;
+	return (this_copy);
+}
+
+Fixed &
+Fixed::min (Fixed &copy1, Fixed &copy2)
+{
+	if (copy1.getRawBits () < copy2.getRawBits ())
+		return (copy1);
+	return (copy2);
+}
+
+const Fixed &
+Fixed::min (Fixed const &copy1, Fixed const &copy2)
+{
+	if (copy1.getRawBits () < copy2.getRawBits ())
+		return (copy1);
+	return (copy2);
+}
+
+Fixed &
+Fixed::max (Fixed &copy1, Fixed &copy2)
+{
+	if (copy1.getRawBits () > copy2.getRawBits ())
+		return (copy1);
+	return (copy2);
+}
+
+const Fixed &
+Fixed::max (Fixed const &copy1, Fixed const &copy2)
+{
+	if (copy1.getRawBits () > copy2.getRawBits ())
+		return (copy1);
+	return (copy2);
+}
 
 int
 Fixed::getRawBits (void) const
--- Module 02/ex02/Fixed.hpp
+++ Module 02/ex02/Fixed.hpp
@@ -11,8 +11,10 @@ class Fixed
 	Fixed (const Fixed &copy);
 	Fixed (const int int_parameter);
 	Fixed (const float float_parameter);
-
 	Fixed &operator= (const Fixed &copy);
+	~Fixed ();
+
+	// Comparison operators
 	bool operator> (const Fixed &copy);
 	bool operator<(const Fixed &copy);
 	bool operator>= (const Fixed &copy);
@@ -20,11 +22,27 @@ class Fixed
 	bool operator== (const Fixed &copy);
 	bool operator!= (const Fixed &copy);
 
-	~Fixed ();
+	// Arithmetic operators
+	float operator+ (const Fixed &copy);
+	float operator- (const Fixed &copy);
+	float operator* (const Fixed &copy);
+	float operator/ (const Fixed &copy);
 
+	// Increment/Decrement operators
+	Fixed &operator++ ();
+	Fixed operator++ (int);
+	Fixed &operator-- ();
+	Fixed operator-- (int);
+
+	// Max/min functions
+	static Fixed &min (Fixed &copy1, Fixed &copy2);
+	static const Fixed &min (Fixed const &copy1, Fixed const &copy2);
+	static Fixed &max (Fixed &copy1, Fixed &copy2);
+	static const Fixed &max (Fixed const &copy1, Fixed const &copy2);
+
+	// Rest of the class
 	int getRawBits (void) const;
 	void setRawBits (int const raw);
-
 	float toFloat (void) const;
 	int toInt (void) const;
 
