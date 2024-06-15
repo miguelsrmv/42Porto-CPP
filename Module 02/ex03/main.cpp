08ab0e9bc3a6fc2a2e96b8a3825106b06c6dd21d
Miguel <miguel.srmv@gmail.com>
2024-06-15 12:06:27 +0100

    Missing BSP logic

--- Module 02/ex03/main.cpp
+++ Module 02/ex03/main.cpp
@@ -1,7 +1,15 @@
-#include "Fixed.hpp"
+#include "Point.hpp"
+
+bool bsp (Point const a, Point const b, Point const c, Point const point);
 
 int
 main (void)
 {
+	const Point a;
+	const Point b;
+	const Point c;
+	const Point point;
+
+	bsp(a, b, c, point);
 	return (0);
 }
