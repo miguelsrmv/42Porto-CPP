b6cc348f34345ce20521fc8800c38fff0aadd631
Miguel <miguel.srmv@gmail.com>
2024-06-18 13:10:53 +0100

    ex00 fix

--- Module 02/ex00/Fixed.cpp
+++ Module 02/ex00/Fixed.cpp
@@ -1,6 +1,10 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed () { std::cout << "Default constructor called" << std::endl; }
+Fixed::Fixed ()
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->fixed_point_number = 0;
+}
 
 Fixed::Fixed (const Fixed &copy)
 {
