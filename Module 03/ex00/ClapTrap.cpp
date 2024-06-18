548aca2185724d43f65c687a4e2d749767c739b8
Miguel <miguel.srmv@gmail.com>
2024-06-18 15:41:30 +0100

    Setting up Constructor and Destructor

--- Module 03/ex00/ClapTrap.cpp
+++ Module 03/ex00/ClapTrap.cpp
@@ -1,3 +1,11 @@
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap(int hit_points, int energy_points, unsigned int attack_data) : hit_points (hit_points), energy_points (energy_points), attack_data (attack_data) {}
+ClapTrap::ClapTrap(int hit_points, int energy_points, unsigned int attack_data) : hit_points (hit_points), energy_points (energy_points), attack_data (attack_data)
+{
+	std::cout << "Constructor has been called" << std::endl;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "Destructor has been called" << std::endl;
+}
