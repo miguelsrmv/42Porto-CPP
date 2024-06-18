7d39a7f17034c5b46631d83446bd142c6108ba9a
Miguel <miguel.srmv@gmail.com>
2024-06-18 15:39:25 +0100

    Before CPP02 eval

---
+++ Module 03/ex00/ClapTrap.cpp
@@ -0,0 +1,3 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(int hit_points, int energy_points, unsigned int attack_data) : hit_points (hit_points), energy_points (energy_points), attack_data (attack_data) {}
