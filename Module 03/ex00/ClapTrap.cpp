5deb8c13ae79583c51952e78f19df36ad161c16b
Miguel <miguel.srmv@gmail.com>
2024-06-18 15:44:10 +0100

    Copy constructor

--- Module 03/ex00/ClapTrap.cpp
+++ Module 03/ex00/ClapTrap.cpp
@@ -2,7 +2,13 @@
 
 ClapTrap::ClapTrap(int hit_points, int energy_points, unsigned int attack_data) : hit_points (hit_points), energy_points (energy_points), attack_data (attack_data)
 {
-	std::cout << "Constructor has been called" << std::endl;
+	std::cout << "Default constructor has been called" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &copy)
+{
+	*this = copy;
+	std::cout << "Copy constructor has been called" <, std::endl;
 }
 
 ClapTrap::~ClapTrap()
