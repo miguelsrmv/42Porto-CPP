2b3898c063dc5d73e99e8ace5372105286529c68
miguel <miguel.srmv@gmail.com>
2024-06-29 17:35:58 +0100

    Ok

--- Module 04/ex01/src/main.cpp
+++ Module 04/ex01/src/main.cpp
@@ -4,9 +4,9 @@
 int
 main (void)
 {
-	Animal *animal_array[100];
+	Animal *animal_array[10];
 
-	for (int i = 0; i < 100; i++)
+	for (int i = 0; i < 10; i++)
 	{
 		std::cout << "(" << i << ")" << std::endl ;
 		if (i % 2)
@@ -15,13 +15,13 @@ main (void)
 			animal_array[i] = new Cat;
 	}
 
-	for (int i = 0; i < 100; i++)
+	for (int i = 0; i < 10; i++)
 	{
 		std::cout << "(" << i << ")" << std::endl ;
 		animal_array[i]->makeSound();
 	}
 
-	for (int i = 0; i < 100; i++)
+	for (int i = 0; i < 10; i++)
 	{
 		std::cout << "(" << i << ")" << std::endl ;
 		delete(animal_array[i]);
