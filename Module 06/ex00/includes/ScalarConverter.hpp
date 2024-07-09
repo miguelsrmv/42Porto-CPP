3881f9ef27fa6889d93baee5c25facdbb77c2729
miguel <miguel.srmv@gmail.com>
2024-07-09 20:40:33 +0100

    Overflow fixed?

--- Module 06/ex00/includes/ScalarConverter.hpp
+++ Module 06/ex00/includes/ScalarConverter.hpp
@@ -1,57 +1,63 @@
 #ifndef SCALAR_CONVERTER_HPP
 #define SCALAR_CONVERTER_HPP
 
-#include <string>
-#include <iostream>
 #include <cctype>
+#include <cstdlib>
+#include <iostream>
 #include <limits>
 #include <sstream>
-#include <cstdlib>
+#include <string>
 
-typedef enum types {
-    CHAR_TYPE,
-    INT_TYPE,
-    FLOAT_TYPE,
-    DOUBLE_TYPE,
-    PSEUDO_LITERAL_TYPE,
-    INVALID_TYPE
+typedef enum types
+{
+	CHAR_TYPE,
+	INT_TYPE,
+	FLOAT_TYPE,
+	DOUBLE_TYPE,
+	PSEUDO_LITERAL_TYPE,
+	INVALID_TYPE
 } types;
 
 class ScalarConverter
 {
-    public:
-        static void convert(std::string& parameter);
+  public:
+	static void convert (std::string &parameter);
 
-    private:
-        // Private Constructors becuase class must not be instanciable by users
-        ScalarConverter();
-        ScalarConverter(const ScalarConverter& copy);
-        const ScalarConverter& operator=(const ScalarConverter& copy);
-        ~ScalarConverter();
-        
-        // Gets type that was inputed
-        static types get_type(const std::string& parameter);
-        static bool is_char(const std::string& parameter);
-        static bool is_int(const std::string& parameter);
-        static bool is_float(const std::string& parameter);
-        static bool is_double(const std::string& parameter);
-        static bool is_pseudo_literal(const std::string& parameter);
+  private:
+	// Private Constructors becuase class must not be instanciable by users
+	ScalarConverter ();
+	ScalarConverter (const ScalarConverter &copy);
+	const ScalarConverter &operator= (const ScalarConverter &copy);
+	~ScalarConverter ();
 
-        // Cverflow checker
-        static bool overflow_check(const char *parameter, types parameter_type);
+	// Gets type that was inputed
+	static types get_type (const std::string &parameter);
+	static bool is_char (const std::string &parameter);
+	static bool is_int (const std::string &parameter);
+	static bool is_float (const std::string &parameter);
+	static bool is_double (const std::string &parameter);
+	static bool is_pseudo_literal (const std::string &parameter);
 
-        // Converts type
-        static void convert_data(const std::string& parameter, long double number);
-        static void convert_char(char c, const std::string& parameter);
-        static void convert_int(int integer, const std::string& parameter);
-        static void convert_float(float floating_point, const std::string& parameter);
-        static void convert_double(double doubling_point, const std::string &parameter);
+	// Cverflow checker
+	static bool overflow_check (const std::string &parameter, types parameter_type);
+    static bool std_conversion_error(float floating_point, const std::string &parameter);
+    static bool std_conversion_error(double doubling_point, const std::string &parameter);
 
-        // Print pseudoliterals
-        static void print_pseudoliterals(const std::string& parameter);
+	// Converts type
+	static void convert_data (const std::string &parameter,
+							  long double number);
+	static void convert_char (char c, const std::string &parameter);
+	static void convert_int (int integer, const std::string &parameter);
+	static void convert_float (float floating_point,
+							   const std::string &parameter);
+	static void convert_double (double doubling_point,
+								const std::string &parameter);
 
-        // Print invalid input message
-        static void print_invalid_input(const std::string& parameter);
+	// Print pseudoliterals
+	static void print_pseudoliterals (const std::string &parameter);
+
+	// Print invalid input message
+	static void print_invalid_input (const std::string &parameter);
 };
 
 #endif
--- Module 06/ex00/src/ScalarConverter.cpp
+++ Module 06/ex00/src/ScalarConverter.cpp
@@ -1,258 +1,313 @@
 #include "ScalarConverter.hpp"
 
-ScalarConverter::ScalarConverter() {}
+ScalarConverter::ScalarConverter () {}
 
-ScalarConverter::ScalarConverter(const ScalarConverter &copy)
-{
-    (void)copy;
-}
+ScalarConverter::ScalarConverter (const ScalarConverter &copy) { (void)copy; }
 
 const ScalarConverter &
-ScalarConverter::operator=(const ScalarConverter &copy)
+ScalarConverter::operator= (const ScalarConverter &copy)
 {
-    (void)copy;
-    return (*this);
+	(void)copy;
+	return (*this);
 }
 
-ScalarConverter::~ScalarConverter() {}
+ScalarConverter::~ScalarConverter () {}
 
-types ScalarConverter::get_type(const std::string &parameter)
+types
+ScalarConverter::get_type (const std::string &parameter)
 {
-    if (is_char(parameter))
-        return CHAR_TYPE;
-    else if (is_int(parameter))
-        return INT_TYPE;
-    else if (is_float(parameter))
-        return FLOAT_TYPE;
-    else if (is_double(parameter))
-        return DOUBLE_TYPE;
-    else if (is_pseudo_literal(parameter))
-        return PSEUDO_LITERAL_TYPE;
-    else
-        return INVALID_TYPE;
+	if (is_char (parameter))
+		return CHAR_TYPE;
+	else if (is_int (parameter))
+		return INT_TYPE;
+	else if (is_float (parameter))
+		return FLOAT_TYPE;
+	else if (is_double (parameter))
+		return DOUBLE_TYPE;
+	else if (is_pseudo_literal (parameter))
+		return PSEUDO_LITERAL_TYPE;
+	else
+		return INVALID_TYPE;
 }
 
-bool ScalarConverter::is_char(const std::string &parameter)
+bool
+ScalarConverter::is_char (const std::string &parameter)
 {
-    // Step #1: Checks for a string of length 1
-    if (parameter.size() != 1)
-        return (false);
+	// Step #1: Checks for a string of length 1
+	if (parameter.size () != 1)
+		return (false);
 
-    // #Step #2: Checks if character is printable
-    if (!std::isprint(parameter[0]))
-        return (false);
+	// #Step #2: Checks if character is printable
+	if (!std::isprint (parameter[0]))
+		return (false);
 
-    // Step #3: Check if character is a digit
-    if (std::isdigit(parameter[0]))
-        return (false);
+	// Step #3: Check if character is a digit
+	if (std::isdigit (parameter[0]))
+		return (false);
 
-    return (true);
+	return (true);
 }
 
-bool ScalarConverter::is_int(const std::string &parameter)
+bool
+ScalarConverter::is_int (const std::string &parameter)
 {
-    // Step #1: Allows for signed numbers
-    size_t i = 0;
-    if (parameter[i] == '+' || parameter[i] == '-')
-        i++;
+	// Step #1: Allows for signed numbers
+	size_t i = 0;
+	if (parameter[i] == '+' || parameter[i] == '-')
+		i++;
 
-    // Step #2: Checks if it's all digits
-    while (parameter[i])
-    {
-        if (!std::isdigit(parameter[i++]))
-            return (false);
-    }
+	// Step #2: Checks if it's all digits
+	while (parameter[i])
+		{
+			if (!std::isdigit (parameter[i++]))
+				return (false);
+		}
 
-    return (true);
+	return (true);
 }
 
-bool ScalarConverter::is_float(const std::string &parameter)
+bool
+ScalarConverter::is_float (const std::string &parameter)
 {
-    // Step #1: Allows for signed numbers
-    size_t i = 0;
-    if (parameter[i] == '+' || parameter[i] == '-')
-        i++;
+	// Step #1: Allows for signed numbers
+	size_t i = 0;
+	if (parameter[i] == '+' || parameter[i] == '-')
+		i++;
 
-    // Step #2: Checks if it finds a float in the correct format
-    /// Step #2.1 Find dot and f in the end
-    size_t dot_index = parameter.find('.');
-    size_t f_index = parameter.find('f');
-    if (dot_index == std::string::npos || f_index == std::string::npos || f_index != parameter.length() - 1)
-        return (false);
+	// Step #2: Checks if it finds a float in the correct format
+	/// Step #2.1 Find dot and f in the end
+	size_t dot_index = parameter.find ('.');
+	size_t f_index = parameter.find ('f');
+	if (dot_index == std::string::npos || f_index == std::string::npos
+		|| f_index != parameter.length () - 1)
+		return (false);
 
-    /// Step #2.2 Find decimal and floating parts
-    std::string integral_part = parameter.substr(i, dot_index - i);
-    std::string fractional_part = parameter.substr(dot_index + 1, f_index - dot_index - 1);
-    if (integral_part.empty() || fractional_part.empty())
-        return (false);
+	/// Step #2.2 Find decimal and floating parts
+	std::string integral_part = parameter.substr (i, dot_index - i);
+	std::string fractional_part
+		= parameter.substr (dot_index + 1, f_index - dot_index - 1);
+	if (integral_part.empty () || fractional_part.empty ())
+		return (false);
 
-    /// Step #2.3 Check that borth decimal and floating parts are made of digits
-    for (size_t index = 0; index < integral_part.length(); index++)
-    {
-        if (!std::isdigit(integral_part[index]))
-            return (false);
-    }
+	/// Step #2.3 Check that borth decimal and floating parts are made of
+	/// digits
+	for (size_t index = 0; index < integral_part.length (); index++)
+		{
+			if (!std::isdigit (integral_part[index]))
+				return (false);
+		}
 
-    for (size_t index = 0; index < fractional_part.length(); index++)
-    {
-        if (!std::isdigit(fractional_part[index]))
-            return (false);
-    }
+	for (size_t index = 0; index < fractional_part.length (); index++)
+		{
+			if (!std::isdigit (fractional_part[index]))
+				return (false);
+		}
 
-    return (true);
+	return (true);
 }
 
-bool ScalarConverter::is_double(const std::string &parameter)
+bool
+ScalarConverter::is_double (const std::string &parameter)
 {
-    // Step #1: Allows for signed numbers
-    size_t i = 0;
-    if (parameter[i] == '+' || parameter[i] == '-')
-        i++;
+	// Step #1: Allows for signed numbers
+	size_t i = 0;
+	if (parameter[i] == '+' || parameter[i] == '-')
+		i++;
 
-    // Step #2: Checks if it finds a float in the correct format
-    /// Step #2.1 Find dot and f in the end
-    size_t dot_index = parameter.find('.');
-    if (dot_index == std::string::npos)
-        return (false);
+	// Step #2: Checks if it finds a float in the correct format
+	/// Step #2.1 Find dot and f in the end
+	size_t dot_index = parameter.find ('.');
+	if (dot_index == std::string::npos)
+		return (false);
 
-    /// Step #2.2 Find decimal and floating parts
-    std::string integral_part = parameter.substr(i, dot_index - i);
-    std::string fractional_part = parameter.substr(dot_index + 1, parameter.length() - dot_index - 1);
-    if (integral_part.empty() || fractional_part.empty())
-        return (false);
+	/// Step #2.2 Find decimal and floating parts
+	std::string integral_part = parameter.substr (i, dot_index - i);
+	std::string fractional_part = parameter.substr (
+		dot_index + 1, parameter.length () - dot_index - 1);
+	if (integral_part.empty () || fractional_part.empty ())
+		return (false);
 
-    /// Step #2.3 Check that borth decimal and floating parts are made of digits
-    for (size_t index = 0; index < integral_part.length(); index++)
-    {
-        if (!std::isdigit(integral_part[index]))
-            return (false);
-    }
+	/// Step #2.3 Check that borth decimal and floating parts are made of
+	/// digits
+	for (size_t index = 0; index < integral_part.length (); index++)
+		{
+			if (!std::isdigit (integral_part[index]))
+				return (false);
+		}
 
-    for (size_t index = 0; index < fractional_part.length(); index++)
-    {
-        if (!std::isdigit(fractional_part[index]))
-            return (false);
-    }
+	for (size_t index = 0; index < fractional_part.length (); index++)
+		{
+			if (!std::isdigit (fractional_part[index]))
+				return (false);
+		}
 
-    return (true);
+	return (true);
 }
 
-bool ScalarConverter::is_pseudo_literal(const std::string &parameter)
+bool
+ScalarConverter::is_pseudo_literal (const std::string &parameter)
 {
-    /// Checks for the valid pseudo literals
-    return (parameter == "-inff" || parameter == "+inff" || parameter == "nanf" || parameter == "-inf" || parameter == "+inf" || parameter == "nan");
+	/// Checks for the valid pseudo literals
+	return (parameter == "-inff" || parameter == "+inff" || parameter == "nanf"
+			|| parameter == "-inf" || parameter == "+inf"
+			|| parameter == "nan");
 }
 
-void ScalarConverter::convert(std::string &parameter)
+void
+ScalarConverter::convert (std::string &parameter)
 {
-    switch (get_type(parameter))
-    {
-    case CHAR_TYPE:
-        return convert_data(parameter, parameter[0]);
-    case INT_TYPE:
-        return convert_data(parameter, std::strtod(parameter.c_str(), NULL));
-    case FLOAT_TYPE:
-        return convert_data(parameter, std::strtof(parameter.c_str(), NULL));
-    case DOUBLE_TYPE:
-        return convert_data(parameter, std::strtod(parameter.c_str(), NULL));
-    case PSEUDO_LITERAL_TYPE:
-        return print_pseudoliterals(parameter);
-    case INVALID_TYPE:
-        return print_invalid_input(parameter);
-    }
+	switch (get_type (parameter))
+		{
+		case CHAR_TYPE:
+			return convert_data (parameter, parameter[0]);
+		case INT_TYPE:
+			return convert_data (parameter, std::atoi (parameter.c_str ()));
+		case FLOAT_TYPE:
+			return convert_data (parameter,
+								 std::strtof (parameter.c_str (), NULL));
+		case DOUBLE_TYPE:
+			return convert_data (parameter,
+								 std::strtod (parameter.c_str (), NULL));
+		case PSEUDO_LITERAL_TYPE:
+			return print_pseudoliterals (parameter);
+		case INVALID_TYPE:
+			return print_invalid_input (parameter);
+		}
 }
 
-void ScalarConverter::convert_data(const std::string &parameter, long double number)
+void
+ScalarConverter::convert_data (const std::string &parameter,
+							   long double number)
 {
-    convert_char(static_cast<char>(number), parameter);
-    convert_int(static_cast<int>(number), parameter);
-    convert_float(static_cast<float>(number), parameter);
-    convert_double(static_cast<double>(number), parameter);
+	convert_char (static_cast<char> (number), parameter);
+	convert_int (static_cast<int> (number), parameter);
+	convert_float (static_cast<float> (number), parameter);
+	convert_double (static_cast<double> (number), parameter);
 }
 
-bool ScalarConverter::overflow_check(const char *parameter, types parameter_type)
+bool
+ScalarConverter::overflow_check (const std::string &parameter,
+								 types parameter_type)
 {
-    long double number = std::strtold(parameter, NULL);
+	long double number = std::strtold (parameter.c_str (), NULL);
 
-    switch (parameter_type)
-    {
-    case CHAR_TYPE:
-        return (number < std::numeric_limits<char>::min() || number > std::numeric_limits<char>::max());
-    case INT_TYPE:
-        return (number < std::numeric_limits<int>::min() || number > std::numeric_limits<int>::max());
-    case FLOAT_TYPE:
-        return (number < -std::numeric_limits<float>::max() || number > std::numeric_limits<float>::max());
-    case DOUBLE_TYPE:
-        return (number < -std::numeric_limits<double>::max() || number > std::numeric_limits<double>::max());
-    default:
-        return false;
-    }
+	switch (parameter_type)
+		{
+		case CHAR_TYPE:
+			return (number < std::numeric_limits<char>::min ()
+					|| number > std::numeric_limits<char>::max ());
+		case INT_TYPE:
+			return (number < std::numeric_limits<int>::min ()
+					|| number > std::numeric_limits<int>::max ());
+		case FLOAT_TYPE:
+			return (number < -std::numeric_limits<float>::max ()
+					|| number > std::numeric_limits<float>::max ());
+		case DOUBLE_TYPE:
+			return (number < -std::numeric_limits<double>::max ()
+					|| number > std::numeric_limits<double>::max ());
+		default:
+			return false;
+		}
 }
 
-void ScalarConverter::convert_char(char c, const std::string &parameter)
+void
+ScalarConverter::convert_char (char c, const std::string &parameter)
 {
-    if (overflow_check(parameter.c_str(), CHAR_TYPE))
-        std::cout << "char: impossible" << std::endl;
-    else if (!isprint(c))
-        std::cout << "char: Non displayable" << std::endl;
-    else
-        std::cout << "char: '" << c << "'" << std::endl;
+	if (overflow_check (parameter, CHAR_TYPE))
+		std::cout << "char: overflow" << std::endl;
+	else if (!isprint (c))
+		std::cout << "char: Non displayable" << std::endl;
+	else
+		std::cout << "char: '" << c << "'" << std::endl;
 }
 
-void ScalarConverter::convert_int(int integer, const std::string &parameter)
+void
+ScalarConverter::convert_int (int integer, const std::string &parameter)
 {
-    if (overflow_check(parameter.c_str(), INT_TYPE))
-        std::cout << "int: impossible" << std::endl;
-    else
-        std::cout << "int: " << integer << std::endl;
+	if (overflow_check (parameter, INT_TYPE))
+		std::cout << "int: overflow" << std::endl;
+	else
+		std::cout << "int: " << integer << std::endl;
 }
 
-void ScalarConverter::convert_float(float floating_point, const std::string &parameter)
+bool
+ScalarConverter::std_conversion_error (float floating_point,
+									   const std::string &parameter)
 {
-    std::string appendix;
-    if (floating_point == static_cast<int>(floating_point))
-        appendix = ".0f";
-    else
-        appendix = "f";
-
-    if (overflow_check(parameter.c_str(), FLOAT_TYPE))
-        std::cout << "float: impossible" << std::endl;
-    else
-        std::cout << "float: " << floating_point << appendix << std::endl;
+	return (floating_point == -1
+			&& std::strtof (parameter.c_str (), NULL) != -1);
 }
 
-void ScalarConverter::convert_double(double doubling_point, const std::string &parameter)
+bool
+ScalarConverter::std_conversion_error (double doubling_point,
+									   const std::string &parameter)
 {
-    std::string appendix;
-    if (doubling_point == static_cast<int>(doubling_point))
-        appendix = ".0";
-    else
-        appendix = "";
-
-    if (overflow_check(parameter.c_str(), DOUBLE_TYPE))
-        std::cout << "double: impossible" << std::endl;
-    else
-        std::cout << "double: " << doubling_point << appendix << std::endl;
+	return (doubling_point == -1
+			&& std::strtod (parameter.c_str (), NULL) != -1);
 }
 
-void ScalarConverter::print_pseudoliterals(const std::string &parameter)
+void
+ScalarConverter::convert_float (float floating_point,
+								const std::string &parameter)
 {
-    std::cout << "char: impossible" << std::endl;
-    std::cout << "int: impossible" << std::endl;
+	std::ostringstream float_stream;
+	float_stream << floating_point;
 
-    if (parameter.find("nan") != std::string::npos)
-    {
-        std::cout << "float: nanf" << std::endl;
-        std::cout << "double: nan" << std::endl;
-    }
-    else
-    {
-        std::cout << "float: " << parameter[0] << "nanf" << std::endl;
-        std::cout << "double: " << parameter[0] << "nan" << std::endl;
-    }
+	if (overflow_check (parameter, FLOAT_TYPE)
+		|| std_conversion_error (floating_point, parameter))
+		std::cout << "float: overflow" << std::endl;
+	else if (float_stream.str ().find ('e') != std::string::npos)
+		std::cout << "float: Non displayable" << std::endl;
+	else
+		{
+			(floating_point == static_cast<int> (floating_point))
+				? float_stream << ".0f"
+				: float_stream << "f";
+			std::cout << "float: " << float_stream.str () << std::endl;
+		}
 }
 
-void ScalarConverter::print_invalid_input(const std::string &parameter)
+void
+ScalarConverter::convert_double (double doubling_point,
+								 const std::string &parameter)
 {
-    std::cout << "Error [invalid input]: " << parameter << std::endl;
+	std::ostringstream double_stream;
+	double_stream << doubling_point;
+
+	if (overflow_check (parameter, DOUBLE_TYPE)
+		|| std_conversion_error (doubling_point, parameter))
+		std::cout << "double: overflow" << std::endl;
+	else if (double_stream.str ().find ('e') != std::string::npos)
+		std::cout << "double: Non displayable" << std::endl;
+	else
+		{
+			(doubling_point == static_cast<int> (doubling_point))
+				? double_stream << ".0"
+				: double_stream << "";
+			std::cout << "double: " << double_stream.str () << std::endl;
+		}
+}
+
+void
+ScalarConverter::print_pseudoliterals (const std::string &parameter)
+{
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: impossible" << std::endl;
+
+	if (parameter.find ("nan") != std::string::npos)
+		{
+			std::cout << "float: nanf" << std::endl;
+			std::cout << "double: nan" << std::endl;
+		}
+	else
+		{
+			std::cout << "float: " << parameter[0] << "inff" << std::endl;
+			std::cout << "double: " << parameter[0] << "inf" << std::endl;
+		}
+}
+
+void
+ScalarConverter::print_invalid_input (const std::string &parameter)
+{
+	std::cout << "Error [invalid input]: " << parameter << std::endl;
 }
