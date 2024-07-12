8775540fa6ac754b3e9de3cd43bd998abe3dd375
Miguel Sá <miguel.srmv@gmail.com>
2024-07-12 19:34:46 +0100

    ex00 done

--- Module 07/ex00/src/main.cpp
+++ Module 07/ex00/src/main.cpp
@@ -1,8 +1,9 @@
-#include <string>
 #include <iostream>
+#include <string>
 
 template <class T>
-void swap(T* a, T* b)
+void
+swap (T *a, T *b)
 {
 	T temp;
 
@@ -11,46 +12,82 @@ void swap(T* a, T* b)
 	*a = temp;
 }
 
-void test_swap()
+void
+test_swap ()
 {
-	std::cout << "Testing with INTS" << std:: endl;
+	std::cout << "Testing with INTS" << std::endl;
 	int a = 1;
 	int b = 2;
 	std::cout << "a is " << a << " and b is " << b << std::endl;
-	swap(&a, &b);
-	std::cout << "a is " << a << " and b is " << b << std::endl;
+	swap (&a, &b);
+	std::cout << "a is " << a << " and b is " << b << std::endl << std::endl;
 
-
-	std::cout << "Testing with FLOATS" << std:: endl;
+	std::cout << "Testing with FLOATS" << std::endl;
 	float c = 10.0f;
 	float d = 20.0f;
 	std::cout << "c is " << c << " and d is " << d << std::endl;
-	swap(&c, &d);
-	std::cout << "c is " << a << " and d is " << d << std::endl;
+	swap (&c, &d);
+	std::cout << "c is " << a << " and d is " << d << std::endl << std::endl;
 
+	std::cout << "Testing with STRINGS" << std::endl;
+	std::string my_name ("Miguel");
+	std::string your_name ("Jonhathan");
+	std::cout << "My name is " << my_name << " and your name is " << your_name
+			  << std::endl;
+	swap (&my_name, &your_name);
+	std::cout << "My name is " << my_name << " and your name is " << your_name
+			  << std::endl
+			  << std::endl;
 
-	std::cout << "Testing with STRINGS" << std:: endl;
-	std::string my_name("Miguel");
-	std::string your_name("Jonhathan");
-	std::cout << "My name is " << my_name << " and your name is " << your_name << std::endl;
-	swap(&my_name, &your_name);
-	std::cout << "My name is " << my_name << " and your name is " << your_name << std::endl;
+	std::cout << std::endl;
 }
 
-void test_min()
+template <class T>
+T
+min (T arg1, T arg2)
 {
-	return ;
+	if (arg1 < arg2)
+		return arg1;
+	return arg2;
 }
 
-void test_max()
+template <class T>
+T
+max (T arg1, T arg2)
 {
-	return ;
+	if (arg1 > arg2)
+		return arg1;
+	return arg2;
 }
 
-int main(void)
+void
+test_min_and_max ()
 {
-	test_swap();
-	test_min();
-	test_max();
+	std::cout << "Testing with INTS" << std::endl;
+	int a = 1;
+	int b = 2;
+	std::cout << min (a, b) << " is lower or equal to " << max (a, b)
+			  << std::endl
+			  << std::endl;
+
+	std::cout << "Testing with FLOATS" << std::endl;
+	float c = 10.0f;
+	float d = 20.0f;
+	std::cout << min (c, d) << " is lower or equal to " << max (c, d)
+			  << std::endl
+			  << std::endl;
+
+	std::cout << "Testing with CHARS" << std::endl;
+	char e = 'e';
+	char f = 'f';
+	std::cout << min (e, f) << " is lower or equal to " << max (e, f)
+			  << std::endl
+			  << std::endl;
 }
 
+int
+main (void)
+{
+	test_swap ();
+	test_min_and_max ();
+}
