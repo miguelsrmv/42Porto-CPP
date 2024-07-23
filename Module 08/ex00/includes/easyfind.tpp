f5a80f36182da77838165084ce84a798d7a59595
Miguel Sá <miguel.srmv@gmail.com>
2024-07-23 20:10:46 +0100

    08/01

--- Module 08/ex00/includes/easyfind.tpp
+++ Module 08/ex00/includes/easyfind.tpp
@@ -1,1 +1,21 @@
 // vim: set filetype=cpp :
+#include "easyfind.hpp"
+
+#include <algorithm>
+#include <iostream>
+
+template <typename T>
+void
+easyfind (T container, int value_to_find)
+{
+	typename T::iterator it;
+
+	it = std::find (container.begin (), container.end (), value_to_find);
+
+	if (it == container.end ())
+		std::cout << "Couldn't find value [" << value_to_find << "]"
+				  << std::endl;
+	else
+		std::cout << "Found value [" << value_to_find << "] on position "
+				  << std::distance (container.begin (), it) << std::endl;
+}
--- Module 08/ex00/src/main.cpp
+++ Module 08/ex00/src/main.cpp
@@ -1,7 +1,37 @@
 #include "easyfind.hpp"
 
+#include <deque>
+#include <list>
+#include <vector>
+
+template <typename T>
+void
+test_container (T &container, const char *container_type)
+{
+	std::cout << "Testing a " << container_type << " container" << std::endl;
+
+	(void)container;
+
+	for (int i = -2; i < 2; i++)
+		container.push_back (i);
+
+	easyfind (container, -1);
+	easyfind (container, 0);
+	easyfind (container, 1);
+	easyfind (container, 999);
+	easyfind (container, -999);
+
+	std::cout << std::endl;
+}
+
 int
 main (void)
 {
-	return (0);
+	std::vector<int> vector_to_test;
+	std::list<int> list_to_test;
+	std::deque<int> deque_to_list;
+
+	test_container (vector_to_test, "vector");
+	test_container (list_to_test, "list");
+	test_container (deque_to_list, "deque");
 }
